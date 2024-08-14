7857a87b1034925bcc85c024342735edf16f0c9f
javadong <dongshaowei1201@gmail.com>
2024-08-14 10:41:35 +0800

    [fix]点位页面修改

--- src/views/manage/node/index.vue
+++ src/views/manage/node/index.vue
@@ -9,21 +9,15 @@
           @keyup.enter="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="所属区域ID" prop="regionId">
-        <el-input
-          v-model="queryParams.regionId"
-          placeholder="请输入所属区域ID"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="合作商ID" prop="partnerId">
-        <el-input
-          v-model="queryParams.partnerId"
-          placeholder="请输入合作商ID"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+      <el-form-item label="区域名称" prop="regionId">
+        <el-select  v-model="queryParams.regionId" placeholder="请选择区域" clearable >
+            <el-option
+              v-for="dict in regionList"
+              :key="dict.id"
+              :label="dict.name"
+              :value="parseInt(dict.id)"
+            ></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -75,20 +69,20 @@
 
     <el-table v-loading="loading" :data="nodeList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="点位ID" align="center" prop="id" />
+      <el-table-column label="序号" type="index" width="50" align="center" prop="id" />
       <el-table-column label="点位名称" align="center" prop="name" />
-      <el-table-column label="所属区域ID" align="center" prop="regionId" />
+      <el-table-column label="所在区域" align="center" prop="region.name" />
       <el-table-column label="商圈类型" align="center" prop="businessType">
         <template #default="scope">
           <dict-tag :options="bussiness_type" :value="scope.row.businessType"/>
         </template>
       </el-table-column>
-      <el-table-column label="合作商ID" align="center" prop="partnerId" />
-      <el-table-column label="详细地址" align="center" prop="address" />
+      <el-table-column label="合作商" align="center" prop="partner.name" />
+      <el-table-column label="详细地址" align="left" prop="address" show-overflow-tooltip/>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['manage:node:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['manage:node:remove']">删除</el-button>
+          <el-button link type="primary" @click="handleUpdate(scope.row)" v-hasPermi="['manage:node:edit']">修改</el-button>
+          <el-button link type="primary" @click="handleDelete(scope.row)" v-hasPermi="['manage:node:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -103,12 +97,19 @@
 
     <!-- 添加或修改点位管理对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
-      <el-form ref="nodeRef" :model="form" :rules="rules" label-width="80px">
+      <el-form ref="nodeRef" :model="form" :rules="rules" label-width="100px">
         <el-form-item label="点位名称" prop="name">
           <el-input v-model="form.name" placeholder="请输入点位名称" />
         </el-form-item>
-        <el-form-item label="所属区域ID" prop="regionId">
-          <el-input v-model="form.regionId" placeholder="请输入所属区域ID" />
+        <el-form-item label="所属区域" prop="regionId">
+          <el-select v-model="form.regionId" placeholder="请选择区域" >
+            <el-option
+              v-for="dict in regionList"
+              :key="dict.id"
+              :label="dict.name"
+              :value="parseInt(dict.id)"
+            ></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="商圈类型" prop="businessType">
           <el-select v-model="form.businessType" placeholder="请选择商圈类型">
@@ -120,8 +121,16 @@
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="合作商ID" prop="partnerId">
-          <el-input v-model="form.partnerId" placeholder="请输入合作商ID" />
+        <el-form-item label="归属合作商" prop="partnerId">
+          <!-- <el-input v-model="form.partnerId" placeholder="请输入合作商ID" /> -->
+           <el-select v-model="form.partnerId" placeholder="请选择合作商">
+             <el-option
+              v-for="dict in partnerList"
+              :key="dict.id"
+              :label="dict.name"
+              :value="parseInt(dict.id)"
+            ></el-option>
+           </el-select>
         </el-form-item>
         <el-form-item label="详细地址" prop="address">
           <el-input v-model="form.address" placeholder="请输入详细地址" />
@@ -139,6 +148,10 @@
 
 <script setup name="Node">
 import { listNode, getNode, delNode, addNode, updateNode } from "@/api/manage/node";
+import { listRegion, getRegion } from "@/api/manage/region";
+import { reactive, ref } from "vue";
+import { listPartner } from "../../../api/manage/partner";
+import { loadAllParams } from "../../../api/page";
 
 const { proxy } = getCurrentInstance();
 const { bussiness_type } = proxy.useDict('bussiness_type');
@@ -226,6 +239,7 @@ function handleQuery() {
 /** 重置按钮操作 */
 function resetQuery() {
   proxy.resetForm("queryRef");
+  queryParams.value.regionId = null;
   handleQuery();
 }
 
@@ -293,5 +307,23 @@ function handleExport() {
   }, `node_${new Date().getTime()}.xlsx`)
 }
 
+/** 获取区域列表 */
+const regionList = ref([]);
+function getRegionList() {
+  listRegion(loadAllParams).then(response => {
+    regionList.value = (response.rows);
+  });
+}
+
+/** 查询合作商列表 */
+const partnerList = ref([]);
+function getPartnerList() {
+  listPartner(loadAllParams).then(response => {
+    partnerList.value = (response.rows);
+  });
+}
+
+getPartnerList();
+getRegionList();
 getList();
 </script>
